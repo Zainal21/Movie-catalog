ab812e76670ae967b50e1f9179f035ece0731323
Zainal21 <Zainalarifin080718@gmail.com>
2022-11-28 15:36:43 +0700

    chore : change apikey into constant

--- app/src/main/java/com/muhamadzain/movie_catalog/network/InterfaceApi.kt
+++ app/src/main/java/com/muhamadzain/movie_catalog/network/InterfaceApi.kt
@@ -2,6 +2,7 @@ package com.muhamadzain.movie_catalog.network
 
 import com.muhamadzain.movie_catalog.model.response.SearchDetailMovieResponse
 import com.muhamadzain.movie_catalog.model.response.SearchMovieResponse
+import com.muhamadzain.movie_catalog.utils.Constant
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,13 +10,13 @@ import retrofit2.http.Query
 interface InterfaceApi {
     @GET("/")
     fun searchMovie(
-        @Query("apikey") apikey : String? = "91b7f888",
+        @Query("apikey") apikey : String? = Constant.API_KEY,
         @Query("s") s: String? = ""
     ) : Observable<SearchMovieResponse>
 
     @GET("/")
     fun getDetailMovie(
-        @Query("apikey") apikey : String? = "91b7f888",
+        @Query("apikey") apikey : String? = Constant.API_KEY,
         @Query("i") t: String? = "",
         @Query("plot") plot: String? = "full"
     ) : Observable<SearchDetailMovieResponse>
